4bd596c7f40fd1518bdcb90e1bc21dad4fb8c971
prattj97 <jessica.pratt@astrazeneca.com>
2021-10-05 16:29:00 +0100

    Changed the boxplot colour and title.

--- R/custom_boxplot.R
+++ R/custom_boxplot.R
@@ -2,6 +2,7 @@ library(ggplot2)
 
 iris_boxplot <- function(){
   
-  ggplot(iris, aes(x = Species, y = Sepal.Length)) +
-    geom_boxplot()
+  ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
+    geom_boxplot() +
+    ggtitle("Iris Species vs Sepal Length")
 }
